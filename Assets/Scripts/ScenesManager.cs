5ac5ee1dada8b1f2a9c366221f0c3d8de169a103
Vasco Gomes <ovascogomes@gmail.com>
2016-08-28 19:40:26 +0100

    Project setup

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScenesManager : MonoBehaviour 
+{
+	public static ScenesManager instance;
+
+    private bool fading = false;
+
+	void Awake()
+	{
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnGUI()
+    {
+        if (fading)
+        {
+
+        }
+    }
+}
